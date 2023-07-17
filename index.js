388411755085482e03c6570343d2ed955ab400d8
shaberry <jaden.png02@gmail.com>
2023-07-17 07:05:39 -0700

    only loops

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-// only odds
+// // only odds
 
-//create variable for number sequence
-// set up array 
-// create for loop
+// //create variable for number sequence
+// // set up array 
+// // create for loop
 
 
-const numbers = [2,4,6,8,11,15,22];
+// // const numbers = [2,4,6,8,11,15,22];
+// // const numbers = [1,2,3,4,5,6,7,8,9,10]
+// // const numbers = [70,42,55,81,21,91,34]
+// const numbers = [2,4,6,8,10,11,12]
 
-const odds = numbers.filter( (num) => num % 2 === 1);
+// const odds = numbers.filter( (num) => num % 2 === 1);
 
-// [11,15]
-console.log(odds);
+// // [11,15]
+// // [1,3,5,7,9]
+// // [55,81,21,91]
+// // [11]
+// console.log(odds);
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// vowel vs constonant
+
+// create input for selected word
 
